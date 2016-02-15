d857275f136d7a69fb3b5c80df01224a623e386c
H. Joe Lee <hyoklee@hdfgroup.org>
2016-02-15 14:24:15 -0600

    access and subset HDF4 with PHP and Apache HTTPD.

---
+++ test_float32.php
@@ -0,0 +1,21 @@
+<?php
+// $range = '60-120';
+$range = '294-297';
+// $range = '298-301';
+$host = "localhost";
+$socket = fsockopen($host,80);
+// $packet = "GET /index.php HTTP/1.1\r\nHost: $host\r\nRange:bytes=$range\r\nAccept-Encoding: gzip\r\nConnection: close\r\n\r\n";
+// $packet = "GET /MOD14CM1.201401.005.01.hdf HTTP/1.1\r\nHost: $host\r\nRange:bytes=$range\r\nAccept-Encoding: gzip\r\nConnection: close\r\n\r\n";
+$packet = "GET /3B43.070901.6A.HDF HTTP/1.1\r\nHost: $host\r\nRange:bytes=$range\r\nAccept-Encoding: gzip\r\nConnection: close\r\n\r\n";
+fwrite($socket,$packet);
+// Read enough packets for header.
+$data = fread($socket, 1024);
+// print($data);
+// print(strlen($data));
+// Last 4 bytes are the first float value.
+$str = substr($data, -4);
+// print($str);
+$val = unpack('f', strrev($str));
+var_dump($val);
+// print(1.0 + $val[1]);
+?>
