727516c1913babc9e808bd8b6c54b68b4008e7b0
Hyo-Kyung (Joe) Lee <hyoklee@giraffe.ad.hdfgroup.org>
2016-08-08 15:12:54 -0500

    EED-60:added MOD08 test.

---
+++ test_opendap.sh
@@ -0,0 +1,5 @@
+# time wget 'http://localhost:8080/opendap/data/hdf4/MOD08_M3.A2014335.006.2015035022023.hdf.dods?Solar_Zenith_Mean_Mean'
+
+time wget 'http://localhost:8080/opendap/data/hdf4/MOD08_M3.A2014335.006.2015035022023.hdf.dods?Solar_Zenith_Mean_Mean,Solar_Zenith_Mean_Std'
+
+time wget 'http://giraffe:8080/opendap/data/hdf4/MOD08_M3.A2014335.006.2015035022023.hdf.das'
